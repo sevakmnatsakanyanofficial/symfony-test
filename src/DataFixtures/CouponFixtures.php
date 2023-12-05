dd954159406cf4e077798c9ae1fdfa9105b7b02f
Mnatsakanyan <mnatsakanyan.sm@gmail.com>
2023-12-05 17:21:46 +0400

    added fixtures

---
+++ src/DataFixtures/CouponFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Coupon;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CouponFixtures extends Fixture
+{
+    private array $coupons = [
+        Coupon::TYPE_PERCENT => 10,
+        Coupon::TYPE_AMOUNT => 200,
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach ($this->coupons as $name => $price) {
+            $product = new Coupon();
+            $product->setType($name);
+            $product->setCode($price);
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
